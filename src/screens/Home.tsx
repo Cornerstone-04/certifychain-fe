43fcbe5a996590cfd107c4d7a3f14a28c7db60de
Cornerstone-04 <fortunecornerstone@gmail.com>
2025-05-07 10:11:56 +0100

    update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,9 @@
-import { ReactNode, useState } from "react";
-import { motion } from "framer-motion";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import AnimatedTabs from "@/components/ui/animated-tabs";
 import UploadTab from "./UploadTab";
 import VerifyTab from "./VerifyTab";
-
-const FadeTab = ({ children }: { children: ReactNode }) => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      {children}
-    </motion.div>
-  );
-};
+import { FadeTab } from "@/components/ui/fade-tab";
+import { useState } from "react";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("upload");
